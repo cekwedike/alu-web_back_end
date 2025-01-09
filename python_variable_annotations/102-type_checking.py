33f8fd8a3439b793cd45666a048045df14415fff
cekwedike <c.ekwedike@alustudent.com>
2025-01-09 13:09:35 +0000

    task 102

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-
 from typing import Tuple, List
 
 def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
-    zoomed_in: List[int] = [
-        item for item in lst
-        for i in range(factor)
-    ]
-    return zoomed_in
+   zoomed_in: List[int] = [
+       item for item in lst
+       for i in range(factor)
+   ]
+   return zoomed_in
 
 array = (12, 72, 91)  # Changed to tuple
 
 zoom_2x = zoom_array(array)
-zoom_3x = zoom_array(array, 3.0)  # This will raise a type error - should be int
+zoom_3x = zoom_array(array, 3)  # Changed 3.0 to 3
