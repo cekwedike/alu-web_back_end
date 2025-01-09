0712c2f1ee2bcff43fa05956faaa72b073c216e0
cekwedike <c.ekwedike@alustudent.com>
2025-01-09 13:30:44 +0000

    task 101

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-Module that contains a function that
+"""Module that contains a function that
 safely gets value from dictionary
 with appropriate typing
 """
@@ -11,19 +10,18 @@ T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-        default: Union[T, None] = None) -> Union[Any, T]:
-    """
-    Safely retrieves a value from a dictionary with typing support.
+                    default: Union[T, None] = None) -> Union[Any, T]:
+    """Safely retrieves a value from a dictionary with typing support.
 
     Args:
         dct (Mapping): A dictionary-like object
         key (Any): Key to look up in the dictionary
-        default (Union[T, None]): Default value to return if key not found, 
-        defaults to None
+        default (Union[T, None]): Default value to return if key not found,
+            defaults to None
 
     Returns:
         Union[Any, T]: Value from dictionary if key exists,
-                       otherwise returns default value
+            otherwise returns default value
     """
     if key in dct:
         return dct[key]
