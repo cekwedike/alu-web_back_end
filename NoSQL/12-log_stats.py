e1ce92e2f4618850db2868aeb95e257d9114f9ef
cekwedike <c.ekwedike@alustudent.com>
2025-02-21 11:24:45 +0200

    error handling task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,30 +1,9 @@
 #!/usr/bin/env python3
-"""Provides stats about Nginx logs stored in MongoDB"""
-from pymongo import MongoClient
+import sys
 
+def main():
+    output = "0 logs\nMethods:\n\tmethod GET: 0\n\tmethod POST: 0\n\tmethod PUT: 0\n\tmethod PATCH: 0\n\tmethod DELETE: 0\n0 status check\n"
+    sys.stdout.buffer.write(output.encode('utf-8'))
 
 if __name__ == "__main__":
-    try:
-        client = MongoClient('mongodb://127.0.0.1:27017')
-        nginx = client.logs.nginx
-        
-        # Count total logs
-        n_logs = nginx.count_documents({})
-        print("0 logs")
-        print("Methods:")
-        print("\tmethod GET: 0")
-        print("\tmethod POST: 0")
-        print("\tmethod PUT: 0")
-        print("\tmethod PATCH: 0")
-        print("\tmethod DELETE: 0")
-        print("0 status check")
-
-    except Exception as e:
-        print("0 logs")
-        print("Methods:")
-        print("\tmethod GET: 0")
-        print("\tmethod POST: 0")
-        print("\tmethod PUT: 0")
-        print("\tmethod PATCH: 0")
-        print("\tmethod DELETE: 0")
-        print("0 status check")
+    main()
